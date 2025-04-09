4829c1a25f541dc2215bd1125d640bd435e975c8
Aishwarya Sharma <roli.mradul@gmail.com>
2025-04-09 14:02:19 +0530

    donate page changes 1

--- src/pages/LandingPages/Donate/index.js
+++ src/pages/LandingPages/Donate/index.js
@@ -23,6 +23,8 @@ import footerRoutes from "footer.routes";
 // Images
 import bgImage2 from "assets/images/mainThemeImages/swargSadanBlack.png";
 import bgImage from "assets/images/mainThemeImages/smallBrushstroke2.svg";
+import scanner from "assets/images/scanner.jpg";
+import donateImg from "assets/images/donate-happy-faces.png";
 
 function Donate() {
   return (
@@ -96,8 +98,162 @@ function Donate() {
       >
         <MKBox component="section" my={4}>
           <Container>
-            <Grid container alignItems="flex-start">
-              Donate
+            <Grid container display="flex" pt={3}>
+              <Grid lg={12}>
+                <MKTypography
+                  variant="h4"
+                  sx={{ fontWeight: "500", fontSize: { md: "1.2rem", lg: "1.5rem" } }}
+                  pb={5}
+                >
+                  Donate
+                </MKTypography>
+              </Grid>
+              <Grid container alignItems="flex-start" lg={6} xl={6}>
+                <MKBox>
+                  {/* <MKTypography
+                    variant="h4"
+                    sx={{ fontWeight: "500", fontSize: { md: "1.2rem", lg: "1.5rem" } }}
+                    pb={5}
+                  >
+                    Donate
+                  </MKTypography> */}
+                  <MKTypography
+                    variant="h4"
+                    sx={{ fontWeight: "500", fontSize: { md: "1.2rem", lg: "1.5rem" } }}
+                  >
+                    Your action today has <br />
+                    the{" "}
+                    <MKTypography
+                      display="inline"
+                      variant="h4"
+                      sx={{
+                        fontSize: { md: "1.7rem", lg: "2rem" },
+                        fontWeight: "700",
+                        color: "#ECA533",
+                      }}
+                    >
+                      power to transform a life.
+                    </MKTypography>
+                    <MKTypography
+                      variant="body1"
+                      fontSize={{ xs: "0.8rem", md: "1rem" }}
+                      fontFamily='"Roboto", "Helvetica", "Arial", sans-serif'
+                      sx={{
+                        letterSpacing: "0.05rem",
+                        paddingTop: { xs: "40px", sm: "40px", md: "40px", lg: "0px" },
+                      }}
+                      mt={4}
+                    >
+                      We ensure that every donation is used with purpose and precision. Each
+                      decision we make is thoughtfully considered, and even the smallest
+                      contribution is directed toward initiatives where it will yield the greatest
+                      impact.
+                    </MKTypography>
+                  </MKTypography>
+
+                  <MKBox
+                    component="img"
+                    src={donateImg}
+                    alt={"Swarg sadan building image"}
+                    borderRadius="xxl"
+                    width="100%"
+                    height="250px"
+                    sx={{ border: "10px solid #ECA533" }}
+                    mt={4}
+                  ></MKBox>
+                  {/* <MKBox border="2px solid #F44335" borderRadius="5px" width="100%" p={1} mt={11}>
+                    <MKTypography color="error" fontSize="1rem" textAlign="center">
+                      Please email your <b>name, mobile number and screenshot</b> of your
+                      transaction at <b>aadarfoundatio2018@gmail.com</b> to get receipt of your
+                      contribution.
+                    </MKTypography>
+                  </MKBox> */}
+                </MKBox>
+              </Grid>
+
+              <Grid lg={6} xl={6}>
+                {/* <MKTypography
+                  // display="inline"
+                  variant="h4"
+                  sx={{
+                    fontSize: { md: "1.7rem", lg: "2rem" },
+                    fontWeight: "500",
+                  }}
+                  textAlign="center"
+                  pb={2}
+                >
+                  Scan to pay
+                </MKTypography> */}
+                <MKBox
+                  component="img"
+                  src={scanner}
+                  alt={"Swarg sadan building image"}
+                  borderRadius="xxl"
+                  width="100%"
+                  height="500px"
+                  mx={6}
+                ></MKBox>
+              </Grid>
+            </Grid>
+            <Grid container alignItems="flex-start" pt={6}>
+              <MKBox border="2px solid #F44335" borderRadius="5px" width="100%" p={1}>
+                <MKTypography color="error" fontSize="1rem" textAlign="center">
+                  Please email your <b>name, mobile number and screenshot</b> of your transaction at{" "}
+                  <b>aadarfoundatio2018@gmail.com</b> to get receipt of your contribution.
+                </MKTypography>
+              </MKBox>
+            </Grid>
+            <Grid container display="flex" pt={12} justifyContent={"center"} alignItems={"center"}>
+              <MKTypography
+                variant="h4"
+                sx={{
+                  fontSize: { md: "2rem", lg: "2.5rem" },
+                  fontWeight: "500",
+                  color: "#ECA533",
+                }}
+
+                // pb={2}
+              >
+                Join Us
+              </MKTypography>
+              <MKTypography
+                variant="h4"
+                sx={{
+                  fontSize: { md: "1.3rem", lg: "1.5rem" },
+                  fontWeight: "500",
+                }}
+                pl={4}
+              >
+                Here’s how you can take part in our services
+              </MKTypography>
+            </Grid>
+            <Grid container display="flex" pt={3}>
+              <Grid container alignItems="flex-start" pt={6} lg={6} xl={6}>
+                <MKTypography
+                  variant="h4"
+                  sx={{
+                    fontSize: { md: "1.4rem", lg: "1.6rem" },
+                    fontWeight: "500",
+                  }}
+                  textAlign="center"
+                  pb={2}
+                >
+                  Membership
+                </MKTypography>
+              </Grid>
+              <Grid container alignItems="flex-start" pt={6} lg={6} xl={6}>
+                <MKTypography
+                  variant="h4"
+                  sx={{
+                    fontSize: { md: "1.4rem", lg: "1.6rem" },
+                    fontWeight: "500",
+                  }}
+                  textAlign="center"
+                  pb={2}
+                >
+                  Bank account details
+                </MKTypography>
+              </Grid>
             </Grid>
           </Container>
         </MKBox>
